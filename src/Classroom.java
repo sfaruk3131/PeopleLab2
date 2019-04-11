584071b2d767e4c348b89bb90f520cfb5a78fe4b
sfaruk3131 <sfaruk3131@bths.edu>
2019-04-11 19:39:57 -0400

    2

---
+++ src/Classroom.java
@@ -0,0 +1,11 @@
+public class Classroom {
+    private Student[] students;
+    private Teacher teacher;
+
+    public Classroom(Student[] students, Teacher teacher)
+    {
+        this.students = students;
+        this.teacher = teacher;
+    }
+
+}
