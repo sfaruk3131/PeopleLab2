3131edd2a203ae6161b737cf4c3086a5acdb4567
sfaruk3131 <sfaruk3131@bths.edu>
2019-04-14 15:37:14 -0400

    3

--- src/Classroom.java
+++ src/Classroom.java
@@ -8,4 +8,29 @@ public class Classroom {
         this.teacher = teacher;
     }
 
+    public String getSubject()
+    {
+        return teacher.getSubject();
+    }
+
+    public double classAverage()
+    {
+        double sum = 0.0;
+        for(int i = 0; i < students.length;i++)
+        {
+             sum = sum + students[i].getGPA();
+        }
+        return (sum / students.length-1);
+    }
+
+    public void printClass()
+    {
+        System.out.println("Teacher: " + this.teacher + " " + getSubject());
+        for(int i = 0; i <students.length;i++)
+        {
+            System.out.println(students[i].getFirstName() + students[i].getFamilyName());
+        }
+        System.out.println("Class Average " + classAverage());
+    }
+
 }
---
+++ src/Runner.java
@@ -0,0 +1,11 @@
+public class Runner {
+    private static String[] firstNames = {"Billy", "John", "Karen", "Joan", "Jennifer"};
+    private static String[] familyNames = {"Johnson", "Baker", "Candy", "Giant", "Crystal"};
+
+    public static void main(String []args)
+    {
+        Teacher teacher = new Teacher();
+
+
+    }
+}
